1aff8b550cb70e87534738d8ab7f3d5e81153e91
Minku Lee <premist@me.com>
2018-02-15 18:57:59 +0900

    Brush up transition and positioning

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationStart } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+import { PhotoDetailComponent } from '../photo-detail/photo-detail.component';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.less']
 })
 export class HeaderComponent implements OnInit {
+  router: Router;
+  black: boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(router: Router) {
+    this.router = router;
   }
 
+  ngOnInit() {
+    this.router.events.pipe(filter(e => e instanceof ActivationStart)).subscribe((e) => {
+      if (e.snapshot.component === PhotoDetailComponent) {
+        this.black = true;
+      } else {
+        this.black = false;
+      }
+    });
+  }
 }
