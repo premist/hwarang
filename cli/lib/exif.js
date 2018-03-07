3050bfe28138e6274d670586e49a91c0d6589aaf
Minku Lee <premist@me.com>
2018-03-07 17:28:17 +0900

    Typo fix

--- cli/lib/exif.js
+++ cli/lib/exif.js
@@ -6,7 +6,7 @@ class Exif {
 
     this.camera = `${this.exif_data.image.Make} ${this.exif_data.image.Model}`;
     this.f_number = this.exif_data.exif.FNumber;
-    this.exposire_time = this.exif_data.exif.ExposureTime;
+    this.exposure_time = this.exif_data.exif.ExposureTime;
     this.iso = this.exif_data.exif.ISO;
     this.focal_length_in_35 = this.exif_data.exif.FocalLengthIn35mmFormat;
     this.lens = this.exif_data.exif.LensModel;
