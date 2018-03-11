357955c1833a9777a0fe5b29973a5dbfd97f45e2
Minku Lee <premist@me.com>
2018-03-11 17:19:52 +0900

    captured_at for exif capture date

--- cli/lib/exif.js
+++ cli/lib/exif.js
@@ -12,6 +12,9 @@ class Exif {
     this.iso = this.exif_data.exif.ISO;
     this.focal_length_in_35 = this.exif_data.exif.FocalLengthIn35mmFormat;
 
+    let cat = this.exif_data.exif.DateTimeOriginal;
+    this.captured_at = Date.parse(cat)/1000;
+
     // Use fraction notication if numerator is not 1
     let et = new Fraction(this.exif_data.exif.ExposureTime);
     this.exposure_time = et.n == 1 ? et.toFraction() : et.toString()
@@ -20,11 +23,12 @@ class Exif {
   simplified() {
     return {
       camera: this.camera,
+      lens: this.lens,
+      captured_at: this.captured_at,
       f_number: this.f_number,
       exposure_time: this.exposure_time,
       iso: this.iso,
-      focal_length_in_35: this.focal_length_in_35,
-      lens: this.lens
+      focal_length_in_35: this.focal_length_in_35
     }
   }
 
