ea18665e080306d56d813b2270c11e832c92e273
Minku Lee <premist@me.com>
2018-03-16 00:41:59 +0900

    Embrace captured_at fully

--- src/app/photo.service.ts
+++ src/app/photo.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
 import { AngularFirestore, DocumentChangeAction } from 'angularfire2/firestore';
+import { DocumentData } from '@firebase/firestore-types';
 
 import { Photo } from './photo';
 
@@ -13,18 +14,18 @@ export class PhotoService {
   constructor(private db: AngularFirestore) { }
 
   getPhotos(): Observable<Photo[]> {
-    return this.db.collection<Photo>('photos', ref => ref.orderBy('created_at', 'desc')).snapshotChanges().map(actions => {
-      return actions.map(this.processPhotoPayload);
-    });
+    return this.db.collection<Photo>('photos', ref => ref.orderBy('created_at', 'desc'))
+      .snapshotChanges()
+      .map(actions => {
+        return actions
+          .map(action => action.payload.doc.data())
+          .map(data => { return Photo.fromDocumentData(data) });
+      });
   }
 
   getPhoto(id: string): Observable<Photo> {
-    return this.db.doc<Photo>(`photos/${id}`).valueChanges();
+    return this.db.doc<DocumentData>(`photos/${id}`).valueChanges()
+      .map(data => { return Photo.fromDocumentData(data) });
   }
 
-  processPhotoPayload(action: DocumentChangeAction): Photo {
-    const data = action.payload.doc.data() as Photo;
-    const id = action.payload.doc.id;
-    return { id, ...data };
-  }
 }
--- src/app/photo.ts
+++ src/app/photo.ts
@@ -1,3 +1,5 @@
+import { DocumentData } from '@firebase/firestore-types';
+
 interface Exif {
   [key: string]: string;
 }
@@ -5,7 +7,27 @@ interface Exif {
 export class Photo {
   id: string;
   title: string;
-  created_at: number;
+  created_at: Date;
+  captured_at: Date;
   original: string;
   exif: Exif;
+
+  captured_at_simple() {
+    let year = this.captured_at.getFullYear();
+    let month = this.captured_at.getMonth()+1;
+    let date = this.captured_at.getDate();
+    return `${year}-${month}-${date}`;
+  }
+
+  static fromDocumentData(documentData: DocumentData): Photo {
+    let photo = new Photo();
+    photo.id = documentData.id;
+    photo.title = documentData.title;
+    photo.created_at = new Date(documentData.created_at*1000);
+    photo.captured_at = new Date(documentData.captured_at*1000);
+    photo.original = documentData.original;
+    photo.exif = documentData.exif;
+
+    return photo;
+  }
 }
