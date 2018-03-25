4773043f9e8c38480aba6a617261c788a7d2fdf0
Minku Lee <premist@me.com>
2018-03-25 21:00:53 +0900

    Add PhotoService#getPhotosFromCollection

--- src/app/photo.service.ts
+++ src/app/photo.service.ts
@@ -13,6 +13,19 @@ export class PhotoService {
 
   constructor(private db: AngularFirestore) { }
 
+  getPhotosFromCollection(collection: string): Observable<Photo[]> {
+    return this.db.collection<Photo>('photos', ref =>
+      ref.where(`collections.${collection}`, '>', 0).orderBy(`collections.${collection}`)
+    )
+      .snapshotChanges()
+      .map(actions => {
+        return actions.map(action => {
+          let doc = action.payload.doc;
+          return Photo.fromDocumentDataWithId(doc.data(), doc.id);
+        });
+      });
+  }
+
   getPhotos(): Observable<Photo[]> {
     return this.db.collection<Photo>('photos', ref => ref.orderBy('created_at', 'desc'))
       .snapshotChanges()
