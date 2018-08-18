e5b1f8c3a1f3b4ee1050f3b8ad0dd0bc8937107c
Minku Lee <premist@me.com>
2018-08-18 19:56:57 +0900

    Update function payload

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,14 +4,57 @@ import { File } from '@google-cloud/storage';
 
 admin.initializeApp();
 
-function generateTempURL(file: File) {
-    const expires_at = new Date();
-    expires_at.setHours(expires_at.getHours() + 2);
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+const bucket = admin.storage().bucket(firebaseConfig.storageBucket);    
 
-    return file.getSignedUrl({
-        action: 'read',
-        expires: expires_at
-    });
+const firestore = admin.firestore();
+firestore.settings({ timestampsInSnapshots: true });
+
+exports.deleteFilesOnDocumentDeletion = functions.firestore.document('/photos/{id}').onDelete((snap, context) => {
+    const data = snap.data();
+    const thumb = bucket.file(getPathFromStorageURL(data.thumbnail));
+    const original = bucket.file(getPathFromStorageURL(data.original));
+
+    return Promise.all([thumb.delete(), original.delete()])
+});
+
+exports.processPublish = functions.database.ref('/publish/{id}').onCreate((snap, context) => {
+    const val = snap.val();
+    const thumb = bucket.file(val.thumb);
+    const large = bucket.file(val.large);
+
+    return thumb.download().then((buffer) => {
+        // Temporary use base64 thumbnail image while the image itself gets copied and made public
+        const encodedThumb = `data:image/jpeg;base64, ${buffer[0].toString('base64')}`;
+
+        return firestore.collection('photos').add({
+            title: 'Upload from Uploader',
+            exif: val.exif,
+            captured_at: new Date(val.captured_at*1000),
+            created_at: new Date(),
+            thumbnail: encodedThumb,
+            original: encodedThumb
+        })    
+    }).then((ref) => {
+        return Promise.all([
+            moveAndMakePublic(thumb, `photos/${ref.id}/thumb.jpg`),
+            moveAndMakePublic(large, `photos/${ref.id}/large.jpg`)
+        ]).then((values) => {
+            const [newThumbPath, newLargePath] = values
+            return ref.update({
+                thumbnail: `https://storage.googleapis.com/${firebaseConfig.storageBucket}/${newThumbPath}`,
+                original: `https://storage.googleapis.com/${firebaseConfig.storageBucket}/${newLargePath}`
+            })
+        })
+    }).then((_) => {
+        return snap.ref.remove()
+    })
+});
+
+
+// Remove prefixes (https://storage.googleapis.com/BUCKETNAME) from URL
+function getPathFromStorageURL(url: string): string {
+    return url.split("/").slice(4).join("/")
 }
 
 function moveAndMakePublic(file: File, destination: string): Promise<string> {
@@ -21,35 +64,3 @@ function moveAndMakePublic(file: File, destination: string): Promise<string> {
         return Promise.resolve(destination)
     })
 }
-
-exports.processPublish = functions.database.ref('/publish/{id}').onCreate((snap, context) => {
-    const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
-    const bucket = admin.storage().bucket(firebaseConfig.storageBucket);    
-
-    const val = snap.val();
-    const thumb = bucket.file(val.thumb);
-    const large = bucket.file(val.large);
-
-    return Promise.all([generateTempURL(thumb), generateTempURL(large)])
-        .then(([thumbUrl, largeUrl]) => {
-            return admin.firestore().collection('photos').add({
-                title: 'Upload from Uploader',
-                exif: val.exif,
-                captured_at: val.captured_at,
-                created_at: val.created_at,
-                thumbnail: thumbUrl[0],
-                original: largeUrl[0]
-            })
-        }).then((ref) => {
-            return Promise.all([
-                moveAndMakePublic(thumb, `photos/${ref.id}/thumb.jpg`),
-                moveAndMakePublic(large, `photos/${ref.id}/large.jpg`)
-            ]).then((values) => {
-                const [newThumbPath, newLargePath] = values.map((v) => v[0])
-                return ref.update({
-                    thumbnail: `https://${firebaseConfig.storageBucket}/${newThumbPath}`,
-                    original: `https://${firebaseConfig.storageBucket}/${newLargePath}`
-                })
-            })
-        })
-});
