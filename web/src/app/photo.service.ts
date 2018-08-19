4ee3698d3732d48f78a09bbf28e24068e6eb8790
Minku Lee <premist@me.com>
2018-08-19 14:34:43 +0900

    Angular version update

--- web/src/app/photo.service.ts
+++ web/src/app/photo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
+import { Observable ,  of, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { AngularFirestore, DocumentChangeAction } from 'angularfire2/firestore';
 import { DocumentData } from '@firebase/firestore-types';
@@ -19,21 +19,21 @@ export class PhotoService {
       ref.where(`collections.${collection}`, '==', true).orderBy('captured_at', 'desc')
     )
       .snapshotChanges()
-      .map(actions => this.actionToPhotos(actions));
+      .pipe(map(actions => this.actionToPhotos(actions)));
   }
 
   getPhotos(): Observable<Photo[]> {
     return this.db.collection<Photo>('photos', ref => ref.orderBy('captured_at', 'desc'))
       .snapshotChanges()
-      .map(actions => this.actionToPhotos(actions));
+      .pipe(map(actions => this.actionToPhotos(actions)));
   }
 
   getPhoto(id: string): Observable<Photo> {
     return this.db.doc<DocumentData>(`photos/${id}`).valueChanges()
-      .map(data => Photo.fromDocumentDataWithId(data, id));
+      .pipe(map(data => Photo.fromDocumentDataWithId(data, id)));
   }
 
-  private actionToPhotos(actions: DocumentChangeAction[]): Photo[] {
+  private actionToPhotos(actions: DocumentChangeAction<Photo>[]): Photo[] {
     return actions.map(action => {
         const doc = action.payload.doc;
         return Photo.fromDocumentDataWithId(doc.data(), doc.id);
--- web/src/app/photo.ts
+++ web/src/app/photo.ts
@@ -14,41 +14,49 @@ export class Photo {
   exif: Exif;
 
   get fNumberFormatted() {
-    if(this.exif.f_number == undefined) { return null; }
+    if (this.exif.f_number === undefined) { return null; }
     return `f/${this.exif.f_number}`;
   }
 
   get capturedAtFormatted() {
-    if(this.captured_at === undefined) { return null; }
+    if (this.captured_at === undefined) { return null; }
 
-    let year = this.captured_at.getFullYear();
-    let month = this.captured_at.getMonth()+1;
-    let date = this.captured_at.getDate();
+    const year = this.captured_at.getFullYear();
+    const month = this.captured_at.getMonth() + 1;
+    const date = this.captured_at.getDate();
     return `${year}-${month}-${date}`;
   }
 
   get exposureTimeFormatted() {
-    if(this.exif.exposure_time === undefined) { return null; }
+    if (this.exif.exposure_time === undefined) { return null; }
     return `${this.exif.exposure_time}s`;
   }
 
   get ISOFormatted() {
-    if(this.exif.iso === undefined) { return null; }
+    if (this.exif.iso === undefined) { return null; }
     return `ISO ${this.exif.iso}`;
   }
 
   static fromDocumentDataWithId(documentData: DocumentData, id: string): Photo {
-    let photo = new Photo();
+    const photo = new Photo();
     photo.id = id;
     photo.title = documentData.title;
-    photo.created_at = new Date(documentData.created_at*1000);
+    photo.created_at = this.parseDate(documentData.created_at);
     if (documentData.captured_at !== null && documentData.captured_at !== undefined) {
-      photo.captured_at = new Date(documentData.captured_at*1000);
+      photo.captured_at = this.parseDate(documentData.captured_at);
     }
     photo.original = documentData.original;
     photo.thumbnail = documentData.thumbnail;
     photo.exif = documentData.exif;
 
     return photo;
+  }
+
+  static parseDate(date: number | Date): Date {
+    if (typeof date === 'number') {
+      return new Date(date * 1000);
+    } else {
+      return date;
+    }
   }
 }
